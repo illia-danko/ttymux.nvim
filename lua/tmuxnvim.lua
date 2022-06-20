3d8db6e6dc78151c5a09d3ac036caefca87af61d
Elijah Danko <me@elijahdanko.net>
2022-06-20 23:36:14 +0200

    Add default_mapping option

--- lua/tmuxnvim.lua
+++ lua/tmuxnvim.lua
@@ -1,20 +1,24 @@
-local collection = require('tmuxnvim.utils.collection')
 local window = require('tmuxnvim.window')
 local config = {}
 
 local function setup(user_config)
-  config = collection.merge_table(config, user_config)
+  config.default_mapping = user_config.default_mapping
+  if config.default_mapping == nil then
+    config.default_mapping = true
+  end
 
   if not vim.env.TMUX then
     return
   end
 
-  local opts = { silent = true, noremap = true }
-  vim.api.nvim_set_keymap("n", "<C-w>l", [[<Cmd>lua require'tmuxnvim.window'.navigate("l")<CR>]], opts)
-  vim.api.nvim_set_keymap("n", "<C-w>h", [[<Cmd>lua require'tmuxnvim.window'.navigate("h")<CR>]], opts)
-  vim.api.nvim_set_keymap("n", "<C-w>k", [[<Cmd>lua require'tmuxnvim.window'.navigate("k")<CR>]], opts)
-  vim.api.nvim_set_keymap("n", "<C-w>j", [[<Cmd>lua require'tmuxnvim.window'.navigate("j")<CR>]], opts)
-  vim.api.nvim_set_keymap("n", "<C-w>c", [[<Cmd>lua require'tmuxnvim.window'.close_current()<CR>]], opts)
+  if config.default_mapping then
+    local opts = { silent = true, noremap = true }
+    vim.api.nvim_set_keymap("n", "<C-w>l", [[<Cmd>lua require'tmuxnvim.window'.navigate("l")<CR>]], opts)
+    vim.api.nvim_set_keymap("n", "<C-w>h", [[<Cmd>lua require'tmuxnvim.window'.navigate("h")<CR>]], opts)
+    vim.api.nvim_set_keymap("n", "<C-w>k", [[<Cmd>lua require'tmuxnvim.window'.navigate("k")<CR>]], opts)
+    vim.api.nvim_set_keymap("n", "<C-w>j", [[<Cmd>lua require'tmuxnvim.window'.navigate("j")<CR>]], opts)
+    vim.api.nvim_set_keymap("n", "<C-w>c", [[<Cmd>lua require'tmuxnvim.window'.close_current()<CR>]], opts)
+  end
 end
 
 return {
